32d69c384a5d40292339219a07a4d3b4e6ae7c0d
Shadow Master <kpriyanshu01programmer@gmail.com>
2025-08-04 00:55:27 +0530

    Did enough work here

---
+++ README.md
@@ -0,0 +1,57 @@
+# Morse Code Translator and Creator
+## Languages Supported
+- ### By default
+  - English
+- ### Not default
+  - Devanagri
+  - Greek
+  - Hebrew
+  - Cyrillic (Russian)
+  - Arabic
+  - Kurdish
+  - Persian
+  - Chinese
+  - Korean
+  - Japanese
+
+## Writing method
+- ### Left to Right
+  - English
+  - Devanagri
+  - Greek
+  - Cyrillic (Russian)
+  - Korean
+- ### Right to Left
+  - Hebrew
+  - Arabic
+  - Kurdish
+  - Persian
+- ### Vertical Column Right to Left
+  - Chinese
+  - Japanese
+
+```C
+struct morse
+{
+  struct morse *parent;         // Will use when extending for different languages, currently to track the parent
+  struct morse *dot;            // Points to next dot morse character
+  struct morse *dash;           // Points to next dash morse character
+  char character;               // Stores the character it represents
+  char last_morse;              // Stores the ending character of morse notation
+}
+```
+NOTE:
+  - Accented characters are introduced in morse.txt file that refer to english only characters as of now, so it is put as blank space character " " (space) for now to let it be skipped
+
+TODO: 
+- create a tree deletion function so as to return the memory used in heap by our program
+- punction marks will take 6-7 characters, modify the code for that accordingly
+- create a makefile to compile code with one command (last thing)
+- create a stack for using user input and pushing it to stack so the initial morse is written backwards, from top to bottom and pop the values to get the correct morse value
+  - create a linked list of all the stacks that will be storing the morse
+  - search the character in the tree and push the morse->last_morse to the stack then move to the parent with morse->parent
+  - continue this till you reach the head, then do this for the other characters
+  - all this is in a linked list of stacks and we pop the first stack and move to the next linked list node for the next character
+  - when the last node is popped, assign the pointer head/temp to NULL
+- as for the morse code to character creation it is easy
+  - traverse the morse code and move to the appropriate character direction, morse->dash or morse->dot and at the final postion, print the character
---
+++ src/morse.c
@@ -0,0 +1,142 @@
+#include "../lib/morse.h"
+
+s_morse *head = NULL;
+
+// Create the node
+s_morse *createNode(s_morse *parent, char last_morse)
+{
+    s_morse *temp = (s_morse *)malloc(sizeof(s_morse));
+    if (!temp)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(memory_allocation_failed);
+    }
+
+    temp->parent = parent;
+    temp->dot = NULL;
+    temp->dash = NULL;
+    temp->character = '\0';
+    temp->last_morse = last_morse;
+    return temp;
+}
+
+s_morse *giveAddress(char *morseCode, int len)
+{
+    s_morse *temp = head;
+    for (int i = 0; i < len - 1; i++)
+    {
+        if (morseCode[i] == '.')
+        {
+            if (temp->dot == NULL)
+                temp->dot = createNode(temp, '.');
+            temp = temp->dot;
+        }
+        else if (morseCode[i] == '-')
+        {
+            if (temp->dash == NULL)
+                temp->dash = createNode(temp, '-');
+            temp = temp->dash;
+        }
+    }
+    return temp;
+}
+
+int insert(char character, char *morseCode, int len)
+{
+    if (character == ' ')
+        character = ' '; // Store actual space
+
+    s_morse *addressPrevious = giveAddress(morseCode, len);
+
+    s_morse **nextPtr = (morseCode[len - 1] == '.') ? &addressPrevious->dot : &addressPrevious->dash;
+
+    if (*nextPtr == NULL)
+        *nextPtr = createNode(addressPrevious, morseCode[len - 1]);
+
+    (*nextPtr)->character = character;
+    return function_ran_success;
+}
+
+// Preorder printing
+void printDataPreOrder(s_morse *p_head)
+{
+    if (p_head == NULL)
+    {
+        return;
+    }
+
+    if (p_head->character != '\0')
+    {
+        if (p_head->character == ' ')
+            printf("' ' (space)\n");
+        else
+            printf("%c\n", p_head->character);
+    }
+
+    printDataPreOrder(p_head->dot);
+    printDataPreOrder(p_head->dash);
+}
+
+// Inorder printing
+void printDataInOrder(s_morse *p_head)
+{
+    if (p_head == NULL)
+    {
+        return;
+    }
+
+    printDataInorder(p_head->dot);
+
+    if (p_head->character != '\0')
+    {
+        if (p_head->character == ' ')
+            printf("' ' (space)\n");
+        else
+            printf("%c\n", p_head->character);
+    }
+
+    printDataInorder(p_head->dash);
+}
+
+void readFile(FILE *file)
+{
+    char buffer[16];
+    char character;
+    char string[morse_max_length];
+    int length = 0;
+
+    while (fgets(buffer, 10, file))
+    {
+
+        length = strlen(buffer); // This is temporarily stored to assign values to appropriate parameters
+        buffer[length - 1] = '\0';
+        length = strlen(buffer);
+        character = buffer[0];
+        for (int i = 0; i < length; i++)
+        {
+            string[i] = buffer[i + 2];
+        }
+        string[length] = '\0';
+        length = strlen(string);
+
+        insert(character, string, length);
+    }
+}
+
+int main()
+{
+    FILE *file;
+    file = fopen("morse.txt", "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "File opening failed\n");
+        exit(file_open_failed);
+    }
+    head = createNode(NULL, '\0');
+
+    readFile(file);
+    printDataPreOrder(head);
+    fclose(file);
+
+    return 0;
+}
